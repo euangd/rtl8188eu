ce0751db88718d42266a6337795763dfac01fadc
euangd <107555110+euangd@users.noreply.github.com>
2023-02-12 14:20:41 +0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -17,10 +17,18 @@ git clone https://github.com/lwfinger/rtl8188eu
 
 # build and install driver
 cd rtl8188eu-master
-make
+make all
 make install
 
 # load the driver module
 modprobe rtl8188eu
 
 echo "Completed installing RTL8188EU drivers,Reboot your pc"
+echo "uncomment the next commands in the sh file to use the troubleshooting steps given in the original repo by lwfinger"
+
+#cp rtl8188eu.bin /lib/firmware/rtlwifi/
+#echo "add [device]"
+#echo "wifi.scan-rand-mac-address=no"
+#echo "the two previous lines of text, need to be on seperate lines, in the file that nano creates next"
+#nano /etc/NetworkManager/conf.d/80-wifi.conf
+#systemctl restart NetworkManager
